c84dfd491a04ea40b39ef53faedef503a08c252a
Rasmus Andersen <rasmus@growupstudio.dk>
2021-08-23 15:54:19 +0200

    second commit

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,62 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import css from '../styles/footer.module.scss'
+import Stoj from '../public/stoj-portrait.jpg'
+import { gsap } from 'gsap';
+import React, { useRef, useState, useEffect } from 'react';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 export default function Footer() {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect (() => {
+
+        var container = gsap.utils.toArray('.container');
+
+        container.forEach((container) => {
+
+            gsap.from(container, {
+            scale: .6,
+                scrollTrigger: {
+                    trigger: container,
+                    start: "top bottom",
+                    end: "bottom bottom",
+                    scrub: true
+                }
+            });
+        });
+
+    }, [])
+
   return (
-    <footer className={css.mainFooter}></footer>
+    <footer className={css.mainFooter}>
+        <div className={`${css.container} container`}>
+            <div className={css.mail}><Link href="mailto:halloj@hallojstoj.dk"><a>halloj@hallojstoj.dk</a></Link></div>
+            <ul className={css.menuList}>
+                <li>
+                    <Link href="/">
+                        <a>Foto</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>Video</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>Galleri</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>Om os</a>
+                    </Link>
+                </li>
+            </ul>
+            <div className={css.copyright}>©2021 STÖJ APS  ·  CVR.: 316237335</div>
+        </div>
+    </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ import css from '../styles/index.module.scss'
 import { gsap } from 'gsap';
 import React, { useRef, useState, useEffect } from 'react';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import Player from '@vimeo/player';
 
 export default function Home() {
 
@@ -23,18 +24,42 @@ export default function Home() {
 
     useEffect (() => {
 
-        gsap.utils.toArray(".scroll").forEach(elem => {
-            gsap.from(elem, {
+        var scroll = gsap.utils.toArray('.scroll');
+
+        scroll.forEach((scroll) => {
+
+            gsap.from(scroll, {
             scale: .6,
                 scrollTrigger: {
-                    trigger: elem,
+                    trigger: scroll,
                     start: "top bottom",
-                    end: "center center",
+                    end: "top center",
                     scrub: true
                 }
             });
         });
+
+        var fadeIn = gsap.utils.toArray('.fadeIn');
+        fadeIn.forEach((fadeIn) => {
+            gsap.from(fadeIn, {
+            opacity: 0,
+                scrollTrigger: {
+                    trigger: fadeIn,
+                    start: "top 70%",
+                    end: "top center",
+                    scrub: true
+                }
+            });
+        });
+
+        const player = new Player('intro-video', {
+            id: 519957973,
+            background: true,
+            responsive: true
+        });
+
     }, [])
+
 
   return (
     <>
@@ -43,32 +68,36 @@ export default function Home() {
         </div>
         <section className={css.mainSection}>
 
-            <div className={`${css.scroll} scroll`} id="kronstadtSkater"><Image src={kronstadtSkater} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="kronstadtSkater"><Image src={kronstadtSkater} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="myMoments"><Image src={myMoments} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="myMoments"><Image src={myMoments} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Marengs"><Image src={Marengs} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Marengs"><Image src={Marengs} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Maze"><Image src={kronstadtMaze} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Vimeo">
+                <div className={`${css.introVideo} fadeIn`} id="intro-video" ></div>
+            </div>
+
+            <div className={`${css.scroll} scroll`} id="Maze"><Image src={kronstadtMaze} layout="responsive" className={`fadeIn`}></Image></div>
 
             <div className={`${css.heading} scroll`} id="heading">
                 <h1>Foto- og Videoproduktion</h1>
                 <p>Vi skaber spændende, visuelle universer, gennem billeder og video. Herunder har vi udvalgt nogle af de seneste projekter, vi har arbejdet på. Vi viser de projekter, der bedst illustrerer hvordan vi arbejder, og de opgaver, der krævede særligt kreative løsninger.</p>
             </div>
 
-            <div className={`${css.scroll} scroll`} id="Femina1"><Image src={feminaOne} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Femina1"><Image src={feminaOne} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Femina2"><Image src={feminaTwo} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Femina2"><Image src={feminaTwo} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Stol"><Image src={Stol} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Stol"><Image src={Stol} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Isabellas1"><Image src={isabellasOne} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Isabellas1"><Image src={isabellasOne} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Isabellas2"><Image src={isabellasTwo} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Isabellas2"><Image src={isabellasTwo} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="is"><Image src={is} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="is"><Image src={is} layout="responsive" className={`fadeIn`}></Image></div>
 
-            <div className={`${css.scroll} scroll`} id="Femina3"><Image src={feminaThree} layout="responsive"></Image></div>
+            <div className={`${css.scroll} scroll`} id="Femina3"><Image src={feminaThree} layout="responsive" className={`fadeIn`}></Image></div>
 
         </section>
     </>
