3de5f497e8ab559cf80b2ab334ea2cd300661297
Rasmus Andersen <rasmus@growupstudio.dk>
2021-11-12 14:31:12 +0100

    Intro animation

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import css from '../styles/index.module.scss'
 import Logo from '../public/logo_white.svg'
 
+import introImage from '../public/intro-image.jpg'
 import firstCase from '../public/Wonder.jpg'
 import secondCase from '../public/kronstadt-skater.jpg'
 import thirdCase from '../public/Mad.jpg'
@@ -69,8 +70,8 @@ export default function Home() {
       scale: .6,
         scrollTrigger: {
           trigger: scroll,
-          start: "top 80%",
-          end: "top 36%",
+          start: "top 95%",
+          end: "top 40%",
           scrub: true
         }
       });
@@ -82,8 +83,8 @@ export default function Home() {
       opacity: 0,
         scrollTrigger: {
           trigger: fadeIn,
-          start: "top 70%",
-          end: "top center",
+          start: "top 85%",
+          end: "top 40%",
           scrub: true
         }
     });
@@ -106,6 +107,7 @@ export default function Home() {
 
       <div className={`${css.introAnimation} ${animateIntro ? '' : css.introAnimationPlayed}`} id="introAnim">
         <h1>Foto, film & halløj</h1>
+        <Image src={introImage} layout='fill' priority='true' objectFit='cover' objectPosition='center' quality='100'/>
       </div>
 
       <div className={css.indexTitle}><span className={css.indexTitleInner} id="indexTitle">Projekter</span></div>
@@ -116,7 +118,7 @@ export default function Home() {
         </div>
 
         <div className={`${css.scroll} scroll`} id="secondCase" style={{ backgroundColor: '#2c99d4' }}>
-          <Image src={secondCase} layout="responsive" className={`fadeIn`} alt=''/>
+          <Image src={secondCase} layout="responsive" className={`fadeIn`} priority='true' alt=''/>
         </div>
 
         <div className={`${css.scroll} scroll`} id="thirdCase" style={{ backgroundColor: '#140c09' }}>
